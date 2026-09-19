b579486446f27daa3adea3392e28f5ea393d1a5e
samal3tvset-sys <samal3tvset@gmail.com>
2026-09-19 18:13:46 +0500

    Create desktop.c

---
+++ desktop.c
@@ -0,0 +1,18 @@
+#include "kernel.c"
+#include "touchscreen.c"
+
+void kernel_main(void) {
+    uart_print("INITIALIZING DESKTOP...\n");
+    desktop_init();
+    touch_init();
+
+    int x, y;
+    while(1) {
+        if (touch_read(&x, &y)) {
+            if (x >= 60 && x <= 180 && y >= 1400 && y <= 1520) {
+                uart_print("APP 1 CLICKED\n");
+            }
+        }
+        __asm__ volatile("wfi");
+    }
+}
